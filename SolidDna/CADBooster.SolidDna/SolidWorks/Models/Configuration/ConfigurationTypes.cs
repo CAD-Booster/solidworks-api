e88a9949a35288024bdc2e13cdc95a92d0ebd1de
CADBooster-Peter <peter@cadbooster.com>
2022-12-27 15:05:28 +0100

    Added configuration properties

---
+++ SolidDna/CADBooster.SolidDna/SolidWorks/Models/Configuration/ConfigurationTypes.cs
@@ -0,0 +1,35 @@
+﻿using SolidWorks.Interop.swconst;
+
+namespace CADBooster.SolidDna.SolidWorks.Models.Configuration
+{
+    /// <summary>
+    /// Types of configurations. Copy of <see cref="swConfigurationType_e"/>
+    /// </summary>
+    public enum ConfigurationTypes
+    {
+        /// <summary>
+        /// A normal configuration
+        /// </summary>
+        Standard = 0,
+
+        /// <summary>
+        /// A top-level weldment configuration
+        /// </summary>
+        AsMachined = 1,
+
+        /// <summary>
+        /// A derived weldment configuration
+        /// </summary>
+        AsWelded = 2,
+
+        /// <summary>
+        /// A flat pattern configuration
+        /// </summary>
+        SheetMetal = 3,
+
+        /// <summary>
+        /// A derived SpeedPak configuration
+        /// </summary>
+        SpeedPak = 4,
+    }
+}

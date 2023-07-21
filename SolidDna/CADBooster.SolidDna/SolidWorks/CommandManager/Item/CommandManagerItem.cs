260f8d1ef270d8d30253f4183d3a7c883e7527f7
CADBooster-Peter <peter@cadbooster.com>
2023-07-21 10:52:01 +0200

    Sorted properties in CommandManagerItem by name

--- SolidDna/CADBooster.SolidDna/SolidWorks/CommandManager/Item/CommandManagerItem.cs
+++ SolidDna/CADBooster.SolidDna/SolidWorks/CommandManager/Item/CommandManagerItem.cs
@@ -13,6 +13,11 @@ namespace CADBooster.SolidDna
         public bool AddSeparatorBeforeThisItem { get; set; }
 
         /// <summary>
+        /// True if the command should be added to the tab
+        /// </summary>
+        public bool AddToTab { get; set; } = true;
+
+        /// <summary>
         /// The command Id of this item (set by the parent)
         /// </summary>
         public int CommandId { get; set; }
@@ -23,25 +28,9 @@ namespace CADBooster.SolidDna
         public string CallbackId { get; } = Guid.NewGuid().ToString("N");
 
         /// <summary>
-        /// The name of the item
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// The position of the item in the list. Specify 0 to add the item to the beginning of the toolbar or menu, or specify -1 to add it to the end.
-        /// After creating the item, we set this to the actual position.
-        /// </summary>
-        public int Position { get; set; } = -1;
-
-        /// <summary>
         /// The hint to show for this item
         /// </summary>
         public string Hint { get; set; }
-
-        /// <summary>
-        /// The tooltip to show for this item
-        /// </summary>
-        public string Tooltip { get; set; }
 
         /// <summary>
         /// The index position of the image to use for this item from the parent image list (zero-index)
@@ -54,14 +43,30 @@ namespace CADBooster.SolidDna
         public CommandItemType ItemType { get; set; } = CommandItemType.MenuItem | CommandItemType.ToolbarItem;
 
         /// <summary>
+        /// The name of the item
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The action to call when the item is clicked
+        /// </summary>
+        public Action OnClick { get; set; }
+
+        /// <summary>
+        /// The position of the item in the list. Specify 0 to add the item to the beginning of the toolbar or menu, or specify -1 to add it to the end.
+        /// After creating the item, we set this to the actual position.
+        /// </summary>
+        public int Position { get; set; } = -1;
+
+        /// <summary>
         /// The tab view style (whether and how to show in the large icon tab bar view)
         /// </summary>
         public CommandManagerItemTabView TabView { get; set; } = CommandManagerItemTabView.IconWithTextBelow;
 
         /// <summary>
-        /// True to show this item in the command tab when a part is open
+        /// The tooltip to show for this item
         /// </summary>
-        public bool VisibleForParts { get; set; } = true;
+        public string Tooltip { get; set; }
 
         /// <summary>
         /// True to show this item in the command tab when an assembly is open
@@ -74,14 +79,9 @@ namespace CADBooster.SolidDna
         public bool VisibleForDrawings { get; set; } = true;
 
         /// <summary>
-        /// The action to call when the item is clicked
+        /// True to show this item in the command tab when a part is open
         /// </summary>
-        public Action OnClick { get; set; }
-
-        /// <summary>
-        /// True if the command should be added to the tab
-        /// </summary>
-        public bool AddToTab { get; set; } = true;
+        public bool VisibleForParts { get; set; } = true;
 
         /// <summary>
         /// Returns a user-friendly string with group properties.

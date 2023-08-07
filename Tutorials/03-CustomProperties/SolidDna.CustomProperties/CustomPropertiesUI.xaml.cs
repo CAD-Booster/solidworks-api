5e4acae7058479c152694e6f4feefc495942acfa
CADBooster-Peter <peter@cadbooster.com>
2023-08-07 18:05:28 +0200

    Small cleanups

--- Tutorials/03-CustomProperties/SolidDna.CustomProperties/CustomPropertiesUI.xaml.cs
+++ Tutorials/03-CustomProperties/SolidDna.CustomProperties/CustomPropertiesUI.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Controls;
 using static CADBooster.SolidDna.SolidWorksEnvironment;
-// ReSharper disable ArrangeMethodOrOperatorBody
 
 namespace SolidDna.CustomProperties
 {
